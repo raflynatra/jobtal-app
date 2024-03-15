67c35e454ea03a69eaeee4bc8f8f8637be3fd178
Rafly Pradana Putra <rafly.pradana@inticorp.tech>
2024-03-15 11:20:47 +0700

    Update hero features

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -41,13 +41,13 @@ function Hero() {
             <input
               type="search"
               id="default-search"
-              className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-emerald-600 focus:border-emerald-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-emerald-600 focus:border-emerald-600"
               placeholder="Search your dream job here ..."
               required=""
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-emerald-600 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white absolute right-2.5 bottom-2.5 bg-emerald-600 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-4 py-2"
             >
               Search
             </button>
